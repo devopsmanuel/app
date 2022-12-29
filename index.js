570560cc60ba239bd7299cd9d68b2c1e2161ae51
devopsmanuel <104746915+devopsmanuel@users.noreply.github.com>
2022-12-29 22:55:54 +0000

    ad code

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongose = require('mongoose');
+const bodyparser = require('body-parser');
+require('dotenv').config()
+
+const app = express();
+
+// capturar body
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+
+// conexión a Base de datos
+
+// import routes
+const authRoutes = require('./routes/auth');
+
+// route middlewares
+app.use('/api/user', authRoutes);
+
+app.get('/', (req, res) => {
+    res.json({
+        estado: true,
+        mensaje: 'funciona!'
+    })
+});
+
+// iniciar server
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`servidor andando en: ${PORT}`)
+});
